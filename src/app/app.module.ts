47b40534edc8842b8a20f442df98c4a8514abcb7
unknown <aloha.hat.2508@gmail.com>
2021-01-14 10:41:18 +0700

    Ghep xong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HelloComponent } from './hello/hello.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {JwtInterceptor} from './helper/jwt-interceptor';
+import {ErrorInterceptor} from './helper/error-interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
-
+import {environment} from '../../environments/environment';
+import {EventEmitter} from 'events';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {UserToken} from '../model/user-token';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+const API_URL = `${environment.apiUrl}`;
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  // @ts-ignore
+  update = new EventEmitter<string>();
+  private currentUserSubject: BehaviorSubject<UserToken>;
+  public currentUser: Observable<UserToken>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<UserToken>(JSON.parse(localStorage.getItem('user')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): UserToken {
+    return this.currentUserSubject.value;
+  }
+
+  // tslint:disable-next-line:typedef
+  login(username: string, password: string) {
+    return this.http.post(API_URL + '/login', {username, password})
+      .pipe(map(user => {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        this.update.emit('login');
+        return user;
+      }));
+  }
+
+  // tslint:disable-next-line:typedef
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSubject.next(null);
+  }
 }
